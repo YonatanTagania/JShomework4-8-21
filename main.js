28dcc890dfb39976e195488976557daccc0f3771
YonatanTagania <87426374+YonatanTagania@users.noreply.github.com>
2021-08-04 23:14:50 +0300

    04/08/21 homework

---
+++ main.js
@@ -0,0 +1,260 @@
+// תרגיל 1
+// var userNumOne = Number(prompt("add a number")),
+//     userNumTwo = Number(prompt("add another number"));
+
+// if (userNumOne + userNumTwo >= 13) {
+//     console.log("bar-mitzva")
+
+// }
+// else if (userNumOne + userNumTwo == 12) {
+//     console.log("bat-mitzva")
+// }
+
+// else {
+//     console.log("kid")
+
+//     תרגיל 2
+//     var yesLoop = 0;
+
+//     while (yesLoop < 100) {
+//         console.log("yes")
+//         yesLoop++
+//     }
+
+//     תרגיל 3
+//     var noLoop = 0; userNumInput = Number(prompt("add a number"))
+//     while (noLoop < userNumInput) {
+//         console.log("NO")
+//         noLoop++
+//     }
+
+//     // תרגיל 4
+//     var jsIsNotJ = 1000;
+//     for (i = 0; i < jsIsNotJ; i++) {
+//         console.log("JavaScript is not Java")
+//     }
+
+//     תרגיל 5
+
+//     var userName = prompt("Add your name"),
+//         userNum = Number(prompt("Add a number"));
+
+//     for (i = 0; i < userNum; i++) {
+//         console.log(`${userName}`)
+//     }
+
+//     תרגיל 6
+//     var userName = prompt("add your name"),
+//         userEmail = prompt("add your email"),
+//         userAge = Number(prompt("add your age"));
+
+//     console.log(userName, userEmail, userAge)
+
+//     var userNumber = Number(prompt("add a number")), printAge = 18;
+
+//     for (i = 0; i < userNumber; i++) {
+
+//         if (userAge >= printAge) {
+//             console.log(`${userName} ${userEmail} ${userAge}`)
+//         }
+//     }
+
+//     תרגיל 7
+//     var userNum = Number(prompt("add a number")), userName = prompt("Add a name");
+
+//     for (i = 0; i < userNum; i++) {
+
+//         if (userName == "Yonatan") {
+//             console.log(`hi boss ${userName}`)
+
+//         }
+
+
+//         else {
+//             console.log("no entry")
+
+//         }
+
+//     }
+
+
+//     תרגיל 8
+
+//     var userNumOne = Number(prompt("add a number")), userNumTwo = Number(prompt("add another number"));
+
+//     for (i = 0; i < userNumOne; i++) {
+//         for (j = 0; j < userNumTwo; j++) {
+
+//         }
+//     }
+
+//     console.log(i * j)
+//     תרגיל 9
+//     var numsArray = [1, 2, 3, 4, 5];
+
+//     console.log(nums)
+
+
+//     // תרגיל 10
+
+//     var namesArray = [`yoni`, `messi`, `yaniv`, `lior`, `moshe`];
+
+//     for (i = 0; i < NumsArray.length; i++) {
+//         console.log(NumsArray[i])
+
+//     }
+
+//     תרגיל 11
+//     var numsArray = [11, 12, 13, 14, 15, 16, 17, 18];
+
+//     for (i = 0; i < numsArray.length; i++) {
+
+//         if (numsArray[i] % 2 == 0) {
+//             console.log(numsArray[i])
+
+//         }
+//     }
+
+//     תרגיל 12
+
+//     var userNames = [];
+
+//     for (i = 0; i < 5; i++) {
+//         userNames[i] = prompt("add a name")
+
+//     }
+//     console.log(userNames)
+
+//     תרגיל 13
+
+//     var userNums = [];
+//     for (i = 0; i < 7; i++) {
+//         userNums[i] = Number(prompt("add a number"))
+//         if (userNums[i] % 2 != 0) {
+//             console.log(`${userNums[i]}`)
+//         }
+//     }
+
+//     תרגיל 14
+//     var mixedArray = ["hello", 22, "good", 4, "day", 69];
+//     for (i = 0; i < mixedArray.length; i++) {
+//         if (mixedArray[i] * 0 != 0) {
+//             mixedArray[i] = "not_number"
+//         }
+//     }
+//     console.log(`${mixedArray}`)
+
+//     תרגיל 15
+
+//     var numsArray = [], i = 0;
+//     while (i < 10) {
+//         var userNum = Number(prompt("add a number"));
+//         if (userNum % 2 == 0) {
+//             numsArray[i] = userNum
+//             i++
+//         }
+
+//     }
+//     console.log(`${numsArray}`)
+
+
+//     תרגיל 16
+
+//     var brickWidth = Number(prompt("add brick width")),
+//         brickHight = Number(prompt("add brick hight"));
+
+//     brickSize = brickWidth * brickHight
+
+//     תרגיל 17
+
+//     var userNumOne = Number(prompt("add a number")), userNumTwo = Number(prompt("add a number")),
+//         userNumThree = Number(prompt("add last number"));
+
+//     if (userNumOne > userNumTwo && userNumOne > userNumThree) {
+//         console.log("higher to lower")
+
+//     }
+//     else if (userNumOne < userNumTwo && userNumOne < userNumThree) {
+//         console.log("lower to higher")
+//     }
+
+//     else if (userNumThree < userNumTwo && userNumThree < userNumOne) {
+//         console.log("lower to higher")
+//     }
+//     else if (userNumThree > userNumTwo && userNumThree > userNumOne) {
+//         console.log("higher to lower")
+//     }
+//     else {
+//         console.log("no order")
+//     }
+
+//     תרגיל 18
+//     var userNumOne = parseInt(Number(prompt("add a number"))),
+//         cast = userNumOne.toString().split('');
+//     for (var i = 0, n = cast.length; i < n; i++) {
+//         console.log(cast[i])
+//     }
+
+// // תרגיל 19
+// var yesLoop1 = 0;
+
+//     while (yesLoop1 < 100) {
+//         console.log("yes")
+//         yesLoop1++
+//     }
+
+    
+// var yesLoop2 = 0;
+
+// while (yesLoop2 < 100) {
+//     console.log("yes")
+//     yesLoop2++
+// }
+
+
+// var yesLoop3 = 0;
+
+//     while (yesLoop3 < 100) {
+//         console.log("yes")
+//         yesLoop3++
+//     }
+
+    
+// var yesLoop4 = 0;
+
+// while (yesLoop4 < 100) {
+//     console.log("yes")
+//     yesLoop4++
+// }
+
+
+// var yesLoop5 = 0;
+
+//     while (yesLoop5 < 100) {
+//         console.log("yes")
+//         yesLoop5++
+//     }
+
+
+    
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+    
+
+
